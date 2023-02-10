35082369152b70e513dd6c18423ba826d5521d96
umenokoudai <umenokoudai0805@gmail.com>
2023-02-10 17:37:56 +0900

    [change] メニューボタンにヒントと回答を追加

--- Assets/Script/MenuButton.cs
+++ Assets/Script/MenuButton.cs
@@ -4,21 +4,23 @@ using Cinemachine;
 
 public class MenuButton : MonoBehaviour
 {
-    [SerializeField, Tooltip("メニューウィンドウ")] GameObject _menu;
+    [SerializeField, Tooltip("メニューウィンドウ")] GameObject _menuWindow;
     [SerializeField, Tooltip("メニューアイコン")] GameObject _menuIcon;
+    [SerializeField, Tooltip("ヒントウィンドウ")] GameObject _hintWindow;
+    [SerializeField, Tooltip("答えウィンドウ")] GameObject _answerWindow;
     [SerializeField] CinemachineVirtualCamera _mainCamera;
 
     //メニューウィンドウを表示　メニューアイコンを非表示
     public void Menu()
     {
-        _menu.SetActive(true);
+        _menuWindow.SetActive(true);
         _menuIcon.SetActive(false);
     }
 
     //メニューウィンドウを非表示　メニューアイコンを表示
     public void Back()
     {
-        _menu.SetActive(false);
+        _menuWindow.SetActive(false);
         _menuIcon.SetActive(true);
     }
 
@@ -33,8 +35,32 @@ public class MenuButton : MonoBehaviour
     {
         SceneManager.LoadScene(SceneName);
     }
-    public void MainCameraBack()
+
+    //ヒント画面を表示
+    public void Hint()
     {
-        _mainCamera.Priority = 12;
+        _menuWindow.SetActive(false);
+        _hintWindow.SetActive(true);
+    }
+
+    //答え画面を表示
+    public void Answer()
+    {
+        _menuWindow.SetActive(false);
+        _answerWindow.SetActive(true);
+    }
+
+    //ヒント画面を閉じる
+    public void HintBack()
+    {
+        _hintWindow.SetActive(false);
+        _menuWindow.SetActive(true);
+    }
+
+    //答え画面を閉じる
+    public void AnswerBack()
+    {
+        _answerWindow.SetActive(false);
+        _menuWindow.SetActive(true);
     }
 }
