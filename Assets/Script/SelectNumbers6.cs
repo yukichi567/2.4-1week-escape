4b1e637f9921cda94acf329b9897dd312ad46113
umenokoudai <umenokoudai0805@gmail.com>
2023-02-11 11:09:10 +0900

    [change] クリア判定の関数を呼び出すようにスクリプト修正

--- Assets/Script/SelectNumbers6.cs
+++ Assets/Script/SelectNumbers6.cs
@@ -3,17 +3,23 @@ using System.Linq;
 
 public class SelectNumbers6 : SingletonMonoBehaviour<SelectNumbers6>
 {
+    Clearjudgment _clearJudgement;
     //ëIëÇµÇΩï∂éöÇäiî[
     string[] _select = new string[4];
     //ìöÇ¶ÇÃï∂éöóÒ
     string[] _answer = { "VII", "II", "IV", "III"};
 
     public string[] Select { get => _select; set => _select = value; }
+    private void Awake()
+    {
+        _clearJudgement = GetComponent<Clearjudgment>();
+    }
     //ê≥âÇ©ÇîªíËÇ∑ÇÈ
     public void Answer()
     {
         if (string.Join("", _select) == string.Join("", _answer))
         {
+            _clearJudgement.ClearJudgment(true);
             Debug.Log("true");
             AudioController.Instance.SePlay(SelectAudio.Success);
         }
