be0752be541545a982326abefe558a67dff934b1
ninomiyahayato <a7pseej9@icloud.com>
2023-02-11 21:40:59 +0900

    2ndstage そこそこ完成

--- Assets/ninomiya/NinoScript.cs
+++ Assets/ninomiya/NinoScript.cs
@@ -7,32 +7,36 @@ using UnityEngine.UI;
 public class NinoScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    List<string> _strList = new List<string>();
-    List<string> _anslist = new List<string>() { "Blue", "Red", "Black", "Green", "Yellow" };
-    [SerializeField] Text _strText;
+    List<string> _strList = new List<string>(); //答え入れるList
+    List<string> _anslist = new List<string>() { "Blue", "Red", "Black", "Green", "Yellow" };　//答え
+    [SerializeField] Text _strText;　//今どの答えが入っているか
+    Clearjudgment _judgment;
     // Start is called before the first frame update
     void Start()
     {
-
+        _judgment = GameObject.FindObjectOfType<Clearjudgment>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_strList.Count == _anslist.Count)
+        if (_strList.Count == _anslist.Count)　//答えのListに5個入力されたときの正解判定
         {
             if (_strList.SequenceEqual(_anslist))
             {
                 Debug.Log("正解しました");
+                AudioController.Instance.SePlay(SelectAudio.Success);
+                _judgment.ClearJudgment(true);
             }
             else
             {
                 _strList.RemoveRange(0, _anslist.Count);
+                AudioController.Instance.SePlay(SelectAudio.Failed);
             }
         }
         _strText.text = string.Join(",", _strList);
     }
-    public void Bottun(int key)
+    public void Bottun(int key)　//Bottunで呼ぶ関数(Dictionary
     {
         var dic = new Dictionary<int, string>()
         {
@@ -45,9 +49,10 @@ public class NinoScript : MonoBehaviour
         if(_strList.Count < 5)
         {
             _strList.Add(dic[key]);
+            AudioController.Instance.SePlay(SelectAudio.Click);
         }
     }
-    public void DeliteKey()
+    public void DeliteKey() //Deliteキーで呼ぶ関数
     {
         if (_strList.Count > 0)
         {
