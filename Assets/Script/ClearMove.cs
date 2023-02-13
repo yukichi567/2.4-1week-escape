64a691a7ab7aad4d5dcc8d00366ed9abfb1f1300
umenokoudai <umenokoudai0805@gmail.com>
2023-02-13 23:45:39 +0900

    タイトルシーンとクリアシーン完成

---
+++ Assets/Script/ClearMove.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class ClearMove : MonoBehaviour
+{
+    [SerializeField]Transform _endScale;
+    [SerializeField] GameObject _buttons;
+    [SerializeField] GameObject _starEffect;
+    Transform _targetObject;
+    void Start()
+    {
+        _targetObject = GetComponent<Transform>();
+        _targetObject.DOScale(_endScale.localScale, 1f).OnComplete(() => _buttons.SetActive(true)).OnComplete(() => Instantiate(_starEffect));
+    }
+}
